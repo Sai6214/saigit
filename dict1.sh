059b981dd3d42cb287b0d224304bc1dc96bc1cab
Saikumar <muthyalasaikumar9@gmail.com>
2022-04-06 17:35:07 +0530

    first commit

---
+++ dict1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+declare -A  addressbook
+addressbook[sai]="123456789"
+addressbook[satish]="123400000"
+addressbook[srinu]="000000000"
+
+echo "contact details" ${addressbook[sai]}
+echo "contacts" ${addressbook[@]}
+echo "all names" ${!addressbook[@]}
+echo "all contact" ${#addressbook[@]}
+
